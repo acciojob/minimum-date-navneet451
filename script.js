e9d84e8fa83b709200481948e1d77791cde9533c
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-16 11:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function minDate(dates) {
-  //write you code here
+  // Convert each date string to a Date object, and then find the minimum date
+  return dates.reduce((min, current) => {
+    return new Date(current) < new Date(min) ? current : min;
+  });
 }
 
 // Do not change the code
